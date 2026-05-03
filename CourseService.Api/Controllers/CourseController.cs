38b3312b837f9c3f074fb4ecf2cce064c06d1594
Vita <bondarenkovita35@gmail.com>
2026-05-03 19:19:22 +0200

    Add project files.

---
+++ CourseService.Api/Controllers/CourseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CourseService.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CourseController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCourses()
+        {
+            return Ok(new string[] { "Course 1", "Course 2" });
+        }
+    }
+}
